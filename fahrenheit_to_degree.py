b2a7a84d407ca8d37d2ac18714e3c5925207e0b5
BenChi3D <benediktchima@gmail.com>
2022-05-02 20:10:16 +0100

    conversion of temperature from fahrenheit to degree celsius modified

--- fahrenheit_to_degree.py
+++ fahrenheit_to_degree.py
@@ -1,3 +1,6 @@
 def fah_to_deg(x):
-    ans = (5/9*x)-32
-    return ans
+    if x > 0:
+        ans = (5/9*x)-32
+        return ans
+    else:
+        print("please input a positive number, cannot evaluate negative numbers")
