442885809932d6ee3cbcb1d91171b1acd26bcb88
BenChi3D <benediktchima@gmail.com>
2022-05-02 22:37:48 +0100

    adding parameters to spiral function

---
+++ 06-spiral-argument.py
@@ -0,0 +1,12 @@
+import turtle
+
+ben = turtle.Turtle()
+
+def spiral(sides, turn, colour, width):
+    ben.color(color)
+    ben.width(width)
+    for side in sides:
+        ben.forward(side)
+        ben.right(turn)
+
+spiral(101, 50, "green", 5) # a test
