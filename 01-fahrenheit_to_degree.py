c20693ea856bbca934d649239bef86900d6b11f6
BenChi3D <benediktchima@gmail.com>
2022-05-03 11:26:19 +0100

    modified fahrenheit to degree temperatures

--- 01-fahrenheit_to_degree.py
+++ 01-fahrenheit_to_degree.py
@@ -1,6 +1,6 @@
 def fah_to_deg(x):
     if x > 0:
-        ans = (5/9)*(x-32)
+        ans = 5 * (x-32) / 9 #  we do this because integer division truncates
         return ans
     else:
         print("please input a positive number, cannot evaluate negative numbers")
