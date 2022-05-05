cfb310ed74396c875a28f93dd3d7fbc452c0d741
BenChi3D <benediktchima@gmail.com>
2022-05-05 19:32:26 +0100

    calculating the area of a triangle function

---
+++ 07-area_tri.py
@@ -0,0 +1,5 @@
+def area_tri():
+    base = input('input a base:\n ')
+    height = input('input a  height:\n ')
+    area = (int(base) * int(height))/2
+    print(f'The area of the triangle is {area}')
