934492d9ddf58b274ae191c172ba3e1327ef7d11
BenChi3D <benediktchima@gmail.com>
2022-05-06 01:18:30 +0100

    making the temperature function accept input

--- 01-fahrenheit_to_degree.py
+++ 01-fahrenheit_to_degree.py
@@ -1,6 +1,9 @@
-def fah_to_deg(x):
-    if x > 0:
-        ans = 5.0 * (x-32.0) / 9.0 #  we do this because integer division truncates
+def fah_to_deg():
+    x = input("enter temperature to be converted to degree celsius:\n ")
+    if int(x) > 0:
+        ans = (5.0 * (int(x)-32.0)) / 9.0 #  we do this because integer division truncates
         return ans
     else:
         print("please input a positive number, cannot evaluate negative numbers")
+
+fah_to_deg()
