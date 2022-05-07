21880be18a5f9b767dd36d4f51eef06de45a458b
BenChi3D <benediktchima@gmail.com>
2022-05-07 22:51:23 +0100

    boolean practice mod

--- 08-BooleanPrac.py
+++ 08-BooleanPrac.py
@@ -1,7 +1,12 @@
-def checkString(word):
+def checkString():
+    word = input("please input password:\n")
     if len(word) == 8 or len(word) > 8:
         print("This is a good password")
-    elif len(word) > 15:
+    elif len(word) >= 15:
         print("The password is too long")
+        checkString()
     else:
         print("Password is too short, please password should be 8 character or more, but less than 15 characters.")
+        checkString()
+
+checkString()
