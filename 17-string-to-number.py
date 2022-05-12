abcfeccaf48dd7bd191843ae9be22d1abc1e41bb
BenChi3D <benediktchima@gmail.com>
2022-05-12 13:40:39 +0100

    Strings to Numbers to Strings

---
+++ 17-string-to-number.py
@@ -0,0 +1,8 @@
+def number():
+    num1 = input("Enter a number:\n")
+    num2 = input("Enter another number:\n")
+    num3 = input("Enter a third number:\n")
+    ans = int(num1) + int(num2) + int(num3)
+    print(f"{num1} + {num2} + {num3} = {ans}")
+
+number()
