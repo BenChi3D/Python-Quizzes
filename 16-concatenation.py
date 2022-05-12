952026a2a5d923c1746226cdb3e37c6a9ef1ba0e
BenChi3D <benediktchima@gmail.com>
2022-05-12 13:21:10 +0100

    concatenation example

---
+++ 16-concatenation.py
@@ -0,0 +1,9 @@
+def conc():
+    print("What's your name?")
+    name = input("\n")
+    print("\nHi, " + name + " It's very nice to meet you.")
+    print("\nWhat's your favorite color?")
+    color = input("\n")
+    print("\nAh, vermillion, " + color + " what a lovely color!")
+
+conc()
