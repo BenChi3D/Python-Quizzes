6c9c51dd2b1e231b8cdd5d8acf1fe0707b178f74
BenChi3D <benediktchima@gmail.com>
2022-05-14 22:22:57 +0100

    general conversion for all integers

--- 01-fahrenheit_to_degree.py
+++ 01-fahrenheit_to_degree.py
@@ -1,9 +1,5 @@
 def fah_to_deg():
     x = input("enter temperature to be converted to degree celsius:\n ")
-    if int(x) > 0:
-        ans = (5.0 * (int(x)-32.0)) / 9.0 #  we do this because integer division truncates
-        return ans
-    else:
-        print("please input a positive number, cannot evaluate negative numbers")
+    return (5.0 * (int(x)-32.0)) / 9.0
 
 fah_to_deg()
